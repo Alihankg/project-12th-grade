5462698925229e436d017cbc6b720f18bc68afe8
alikhan <kebrun2017@gmail.com>
2023-12-24 20:00:47 +0300

    Changed design and moved to modal based crud functions

--- app/settings.py
+++ app/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External
     'bootstrap5',
+    'fontawesomefree',
+    # Internal
     'reservations'
 ]
 
--- reservations/views.py
+++ reservations/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .forms import SubjectForm
 from .models import Subject, Teacher
 from django.contrib import messages
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='login')
 def subject_list(req):
     subjects = Subject.objects.all()
+    subject_form = SubjectForm()
     context = {
-        'subjects': subjects
+        'subjects': subjects,
+        'form': subject_form,
     }
     return render(req, 'subject_list.html', context)
 
@@ -20,30 +23,19 @@ def subject_create(req):
         form.save()
         messages.success(req, 'Ders başarılı bir şekilde eklendi.')
         return redirect('subjects')
-    
-    context = {'form': form}
-    return render(req, 'subject_create.html', context)
 
 @login_required(login_url='login')
 def subject_update(req, id):
     subject = get_object_or_404(Subject, id = id)
-    form =  SubjectForm(req.POST or None, instance=subject)
+    form = SubjectForm(req.POST or None, instance=subject)
     if form.is_valid():
         form.save()
         messages.success(req, 'Ders başarılı bir şekilde güncellendi.')
         return redirect('subjects')
-    context = {
-        'form': form
-    }
-    return render(req,'subject_update.html', context)
 
 @login_required(login_url='login')
 def subject_delete(req, id):
     subject = get_object_or_404(Subject, id = id)
- 
-    if req.method =="POST":
-        subject.delete()
-        messages.success(req, 'Ders başarılı bir şekilde silindi.')
-        return redirect('subjects')
-    
-    return render(req, 'subject_delete.html')
+    subject.delete()
+    messages.success(req, 'Ders başarılı bir şekilde silindi.')
+    return redirect('subjects')
